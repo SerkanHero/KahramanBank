457f45abb1801d3f444b73761c54f9d9f3e11cfc
SerkanHero <152750036+SerkanHero@users.noreply.github.com>
2023-12-03 03:21:12 +0300

    Add files via upload

---
+++ SerkanK/Database/SystemDBContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using SerkanK.Models;
+
+namespace SerkanK.Database
+{
+    public class SystemDBContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Card> Cards { get; set; }
+
+        public bool StartUpCheck = false;
+
+        public SystemDBContext(DbContextOptions<SystemDBContext> options) : base (options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<Account>().ToTable("Accounts");
+            modelBuilder.Entity<Transaction>().ToTable("Transactions");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=bank.db");
+        }
+
+        public void StartUp()
+        {
+            if(!StartUpCheck)
+                this.Database.EnsureCreated();
+            StartUpCheck = true;
+        }
+
+    }
+}
